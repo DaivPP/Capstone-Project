52c9764e6a9f0546d9c106438c3346d6265ea7d1
Aditya Rajesh <adityarajesh1122@gmail.com>
2025-08-22 13:16:13 +0530

    Delete src directory

--- src/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function ProtectedRoute({ children }) {
-  const isAuth = localStorage.getItem("authToken"); // fake auth check
-  return isAuth ? children : <Navigate to="/" />;
-}
-
-export default ProtectedRoute;
